dbd5903c217eaf6d787ca2dbab86737b364fdf5c
priya <priyadagar0044@gmail.com>
2026-07-19 22:21:05 +0530

    Fix unused import build error

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,4 @@
-import { Routes, Route, Navigate } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import { AuthProvider } from "./context/AuthContext";
 import ProtectedRoute from "./components/ProtectedRoute";
 import Layout from "./components/Layout";
